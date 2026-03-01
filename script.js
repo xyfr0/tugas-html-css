d9aede0a82c65e049e5c4449089c9d7aa7483d2a
xyfr0 <jabbarirhamabdul0@gmail.com>
2026-03-02 01:09:58 +0700

    add pages

---
+++ script.js
@@ -0,0 +1,13 @@
+const answers = document.querySelectorAll('#answer-box');
+
+answers.forEach(answer => {
+    answer.addEventListener('click', function() {
+        const isCorrect = this.getAttribute('data-answer');
+
+        if(isCorrect === 'correct'){
+            window.location.href = 'correct.html'
+        }else{
+            window.location.href = 'wrong.html'
+        }
+    })
+})
